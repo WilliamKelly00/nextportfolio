ee865dcd39387eb091002f58c746f9b1e8ee9936
WilliamKelly00 <williammkelly00@gmail.com>
2022-02-06 17:37:31 -0500

    added logo, adding cards

---
+++ components/Cards.js
@@ -0,0 +1,52 @@
+import styles from '../styles/Card.module.css'
+
+export default function Cards() {
+  return(
+    <div className={styles.bodyC}>
+    <div className={styles.container}>
+        <div className={styles.card}>
+            <div className={styles.face-face1}>
+                <div className={styles.content}>
+                    <img src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/design_128.png?raw=true" />
+                    <h3>Design</h3>
+                </div>
+            </div>
+            <div className={styles.face-face2}>
+                <div className={styles.content}>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cum cumque minus iste veritatis provident at.</p>
+                        <a href="#">Read More</a>
+                </div>
+            </div>
+        </div>
+        <div className={styles.card}>
+            <div className={styles.face-face1}>
+                <div className={styles.content}>
+                    <img src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/code_128.png?raw=true" />
+                    <h3>Code</h3>
+                </div>
+            </div>
+            <div className={styles.face-face2}>
+                <div className={styles.content}>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cum cumque minus iste veritatis provident at.</p>
+                        <a href="#">Read More</a>
+                </div>
+            </div>
+        </div>
+        <div className={styles.card}>
+            <div className={styles.face-face1}>
+                <div className={styles.content}>
+                    <img src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/launch_128.png?raw=true" />
+                    <h3>Launch</h3>
+                </div>
+            </div>
+            <div className={styles.face-face2}>
+                <div className={styles.content}>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cum cumque minus iste veritatis provident at.</p>
+                        <a href="#">Read More</a>
+                </div>
+            </div>
+        </div>
+  </div>
+  </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Cards from '../components/cards'
 
 export default function Home() {
   return (
@@ -9,24 +10,24 @@ export default function Home() {
     <Head>
         <title>William Kelly</title>
         <meta name="description" content="William Kelly portfolio" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.svg" />
       </Head>
 
       <div className={styles.body}>
         <div className={styles.svgoverlay}>
           <svg width="80" height="80" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M30 64.1641L50 16.1641" stroke="#C2CCDE" stroke-linecap="round" stroke-linejoin="round" />
-            <path d="M23 24.1641L5.84089 39.4167C5.39337 39.8144 5.39337 40.5137 5.84088 40.9115L23 56.1641" stroke="#C2CCDE" stroke-linecap="round" stroke-linejoin="round" />
-            <path d="M57 24.1641L74.1592 39.4167C74.6067 39.8144 74.6067 40.5137 74.1592 40.9115L57 56.1641" stroke="#C2CCDE" stroke-linecap="round" stroke-linejoin="round" />
+            <path className={styles.svgLine} d="M30 64.1641L50 16.1641" stroke="#4d4d4d" stroke-linecap="round" stroke-linejoin="round" />
+            <path className={styles.svgLine} d="M23 24.1641L5.84089 39.4167C5.39337 39.8144 5.39337 40.5137 5.84088 40.9115L23 56.1641" stroke="#4d4d4d" stroke-linecap="round" stroke-linejoin="round" />
+            <path className={styles.svgLine} d="M57 24.1641L74.1592 39.4167C74.6067 39.8144 74.6067 40.5137 74.1592 40.9115L57 56.1641" stroke="#4d4d4d" stroke-linecap="round" stroke-linejoin="round" />
           </svg>
         </div>
-
             <video id={styles.video} autoPlay loop muted>
                 <source src="/land.mov" type='video/mp4' />
             </video>
       </div>
 
-
+    <Cards/>
+    
     </div>
   )
 }
