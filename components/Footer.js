d2a6f808aee02b597d6511ea2102d465bd5f3466
WilliamKelly00 <williammkelly00@gmail.com>
2022-02-13 23:22:01 -0500

    added footer

---
+++ components/Footer.js
@@ -0,0 +1,13 @@
+import styles from '../styles/Footer.module.css'
+
+export default function Footer() {
+  return (
+    <footer>
+        <div className={styles.copyright}>
+            <p>© 2022 William Kelly, LP. All rights reserved.</p>
+            <a href="https://github.com/WilliamKelly00/nextportfolio">view code</a>
+        </div>
+        <div className={styles.gradient}></div>
+    </footer>
+  )
+}
