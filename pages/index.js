5c6ebc9ae1aa0db5e4fb07e7c6a367b00f69b4e4
WilliamKelly00 <williammkelly00@gmail.com>
2022-02-21 15:14:31 -0500

    added responsive meta tag

--- pages/index.js
+++ pages/index.js
@@ -5,23 +5,27 @@ import TwoWaves from '../components/TwoWaves';
 import Landing from '../views/Landing';
 import Projects from '../views/Projects'
 import Contact from '../views/Contact'
+import styles from '../styles/Home.module.css'
 
 export default function Home() {
 
   return (
    <div> 
-    <Head>
+      <Head>
         <title>William Kelly</title>
         <meta name="description" content="William Kelly portfolio" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/favicon.svg" />
       </Head>
 
+    <div className={styles.container}>
       <Landing/>
       <Projects/>
       <TwoWaves/>
       <Cards/>      
       <Contact/>
       <Footer/>
+    </div>
 
     </div>
   )
